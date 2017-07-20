4a722812697ecc2daf7ac1e9dcbc31c52a15428b
Liudas Baksys <liudasbk@users.noreply.github.com>
2017-07-20 18:03:03 +0300

    Removes tcp-nodelay which brakes tests on debian

--- test/cookbooks/qpidd_test/recipes/default.rb
+++ test/cookbooks/qpidd_test/recipes/default.rb
@@ -6,6 +6,11 @@ end
 qpid_service 'second' do
   port '5673'
   interface '127.0.0.1'
-  variables({ 'tcp-nodelay' => true, 'auth' => true, 'max-connections' => 100 })
+  variables({ 'auth' => true, 'max-connections' => 100 })
   action [:create, :start]
 end
+
+# qpid_service 'delete' do
+#   port '5674'
+#   action [:create, :delete]
+# end
